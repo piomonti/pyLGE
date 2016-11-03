c108e56441ec10932f7a7d258d2f29d222151d14
Ricardo Pio Monti <ricardo.monti08@gmail.com>
2016-11-03 16:49:44 +0000

    improved figure

--- example.py
+++ example.py
@@ -7,22 +7,28 @@
 import numpy, os, pandas
 import cPickle as pickle
 import pylab as plt
+import matplotlib.collections as collections
 from pyLGE_02 import *
 
 
 os.chdir('Data/')
 Nets = pickle.load(open('ToyNetworks.p', 'rb'))
 
+# prepare image:
+f, (ax1, ax2) = plt.subplots(1, 2)
+t = numpy.linspace(1, 300,300)
 
 ### run PCA embedding:
 Lpca = LGE(Adj = Nets, EmbedMethod='PCA', k=1)
 ResPCA = Lpca.embed()
 
-plt.plot( ResPCA[1].mean(axis=1), label='In-sample embedding')
-plt.plot( ResPCA[2].mean(axis=1), label='Out-of-sample embedding')
-plt.legend()
-plt.title('PCA embedding')
-plt.xlabel('Time')
+ax1.plot( ResPCA[1].mean(axis=1), label='In-sample embedding', linewidth=2.5)
+ax1.plot( ResPCA[2].mean(axis=1), label='Out-of-sample embedding', linewidth=2.5)
+ax1.legend()
+ax1.set_title('PCA embedding', fontsize=25)
+ax1.set_xlabel('Time')
+collection = collections.BrokenBarHCollection.span_where(t, ymin=-1, ymax=1, where=((t<100) | (t>200)), facecolor='blue', alpha=0.25, label='0 back')
+ax1.add_collection(collection)
 
 ### run LDA embedding:
 # start by building response vector which is required:
@@ -36,12 +42,13 @@ ClassFull = numpy.tile( ClassVec.T, len(Nets)).T
 Llda = LGE(Adj=Nets, EmbedMethod='LDA', classID=ClassFull) 
 ResLDA = Llda.embed()
 
-plt.plot( ResLDA[1].mean(axis=1), label='In-sample embedding')
-plt.plot( ResLDA[2].mean(axis=1), label='Out-of-sample embedding')
-plt.legend(loc='best')
-plt.title('LDA embedding')
-plt.xlabel('Time')
-
+ax2.plot( ResLDA[1].mean(axis=1), label='In-sample embedding', linewidth=2.5)
+ax2.plot( ResLDA[2].mean(axis=1), label='Out-of-sample embedding', linewidth=2.5)
+ax2.legend(loc='best')
+ax2.set_title('LDA embedding', fontsize=25)
+ax2.set_xlabel('Time')
+collection = collections.BrokenBarHCollection.span_where(t, ymin=-1, ymax=1, where=((t<100) | (t>200)), facecolor='blue', alpha=0.25, label='0 back')
+ax2.add_collection(collection)
 
 
 
